7675a03231b77910dd57e62f22cd2837bcbeeff9
Matthew Lawrence <mattblawr@gmail.com>
2020-02-10 20:22:02 -0800

    Add bullet functionality to towers
    Handled by Tower and Bullet classes

---
+++ New Unity Project/Assets/Scripts/Tower/Bullet.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 1f;
+    public int damage = 1;
+
+    Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!target) {
+            Destroy(gameObject); return;
+        }
+
+        // Follow target
+        transform.Translate((target.position - transform.position).normalized * speed);
+
+        if(Vector3.Distance(target.position, transform.position) < 0.5f)
+        {
+            HitTarget(target);
+        }
+    }
+
+    // Collision isn't working?!
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        HitTarget(other.transform);
+    }
+
+    public void SetTarget(Transform target)
+    {
+        this.target = target;
+    }
+
+    void HitTarget(Transform hit)
+    {
+        Enemy enemy = hit.transform.GetComponent<Enemy>();
+
+        // Deal damage
+        if (enemy)
+        {
+            enemy.TakeDamage(damage);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Tower/Tower.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    //for all towers
+    public float radius;
+    public float attackDelay = 1f;
+
+    public Bullet bulletPrefab;
+
+    // Store all enemies in range
+    private List<Enemy> enemyList = new List<Enemy>();
+
+    private Vector3 position; // this will be the center
+    private CircleCollider2D towerRange; 
+    private float timer = 0f;
+
+    // Checks for enemies that enter the radius
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Enemy enemy = collision.transform.GetComponent<Enemy>();
+
+        //add enemy to the target list
+        if (enemy)
+        {
+            enemyList.Add(enemy);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Enemy enemy = collision.transform.GetComponent<Enemy>();
+
+        if (enemy)
+        {
+            enemyList.Remove(enemy);
+        }
+    }
+
+    // Fire a bullet at the first enemy in the list
+    private void AttackEnemy()
+    {
+        Enemy enemy = enemyList[0];
+
+        if (!bulletPrefab) { return; }
+
+        Bullet bullet = GameObject.Instantiate(bulletPrefab, position, Quaternion.identity);
+        bullet.SetTarget(enemy.transform);
+
+        // bullet will yeet
+        Destroy(bullet.gameObject, 3f);
+    }
+
+    //----------------------------------------------------------------------------
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position;
+        towerRange = GetComponent<CircleCollider2D>();
+        towerRange.radius = radius;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        // If there are enemies to shoot...
+        if ((enemyList.Count != 0) && (timer >= attackDelay)) {
+            // Shoot at them
+            AttackEnemy();
+
+            // Reset cooldown
+            timer = 0;
+        }
+    }
+}
