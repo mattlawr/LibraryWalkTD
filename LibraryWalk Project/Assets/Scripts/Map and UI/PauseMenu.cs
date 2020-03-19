93792d95c4fb420a88ce9be95b6978e2d0d6e687
mattlawr <mattblawr@gmail.com>
2020-03-19 14:37:16 -0700

    Remove old folder, rename project folder
    It had to be done

---
+++ LibraryWalk Project/Assets/Scripts/Map and UI/PauseMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gamePaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if p key is pressed, then either pause or resume the game
+        if (Input.GetKeyDown(KeyCode.P) && gamePaused)
+        {
+            Resume();
+        }
+        else if (Input.GetKeyDown(KeyCode.P) && !gamePaused)
+        {
+            Pause();
+        }
+    }
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gamePaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gamePaused = true;
+    }
+}
